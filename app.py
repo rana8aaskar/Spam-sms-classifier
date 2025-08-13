1742f0686e8443f5a71ad7a79e562868ec87752a
Aaskar Rana <aaskar04rana@gmail.com>
2025-08-13 19:27:06 +0530

    downloaded punkt_tab

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import nltk
 nltk.download('punkt')
+nltk.download('punkt_tab')
 import string
 from nltk.corpus import stopwords
 from nltk.stem.porter import  PorterStemmer
